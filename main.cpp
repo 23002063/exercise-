29aedead70cf1d7cc710951ce3b764cfd779edde
23002063 <126182871+23002063@users.noreply.github.com>
2024-04-18 19:58:36 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+cout<<"enter your age"<<endl;
+std::cin>>age;
+if(age>=18)
+    std::cout<<"fit the description"<<endl;
+else if(age<=25)
+   std:: cout<<"fit the description"<<endl;
+else
+    std::cout<<"do not fit the description"<<endl;
+int weight;
+std::cout<<"enter your weight"<<endl;
+std::cin>>weight;
+
+  if(weight>= 70)
+    std::cout<<"fit the description"<<endl;
+  else if(weight<=80)
+    std::cout<<"fit the description"<<endl;
+  else
+    std::cout<<"do not fit the description"<<endl;
+
+    return 0;
+
+}
